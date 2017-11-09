ef3ec7544519e1922b0477c861815d2832a742fe
wellington <wellington@192.168.107.197>
2017-11-09 20:29:20 -0200

    adiçao de comentarios

--- src/entidade/EContaCorrente.java
+++ src/entidade/EContaCorrente.java
@@ -10,12 +10,12 @@ public class EContaCorrente {
         this.saldo = saldo;
     }
 
-    
-    
+   
+   //Metodo get para obter saldo 
     public Double getSaldo() {
         return saldo;
     }
-
+//Metodo set para setar saldo
     public void setSaldo(Double saldo) {
         if(saldo == null) {
             throw new IllegalArgumentException("não é permitido saldo nulo");
